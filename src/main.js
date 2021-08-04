f582be3b96b7c9fbde4946b3c7ee8a22d617321b
nick <nickbarry@163.com>
2021-08-04 10:14:59 +0800

    1、优化登录界面login.vue的UI 2、由于App.vue中的css样式会影响Login.vue的UI，所以注释掉了 3、main.js中导入element-ui（通过npm下载）

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+
 // 设置反向代理，前端请求默认发送到http://localhost:8443/api
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
 // 全局注册，之后可以再其他组件中通过this.$axios发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
